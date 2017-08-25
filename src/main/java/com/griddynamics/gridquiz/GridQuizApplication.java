a838d657c42d8dddebb775488342cb19f1fbe5e4
Goga <tirkiya@icloud.com>
2017-08-25 20:07:58 +0400

    Setting up environment

---
+++ src/main/java/com/griddynamics/gridquiz/GridQuizApplication.java
@@ -0,0 +1,12 @@
+package com.griddynamics.gridquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridQuizApplication.class, args);
+	}
+}
